7fd811848650147789c417956a41b6b69f99f4d6
Carlos Morales <carlos.rey.morales@gmail.com>
2020-10-14 15:55:01 -0400

    fixing another bug for f3

--- f3.js
+++ f3.js
@@ -1,5 +1,6 @@
 const f3 = 'feature 3';
 const f3StillWorking = 'still working on f3 here';
 const f3FixingBug = 'I fixed a bug QA Found';
+const f3FixingYetAnotherBug = 'fixing another bug';
 
 export default f3;
